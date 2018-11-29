d7038d2071b30d3ff93158c1cf3b12afe6bd06e0
Mikael <prizelive@live.dk>
2018-11-29 18:39:27 +0000

    Switched to python

--- main.rkt
+++ main.rkt
@@ -11,30 +11,32 @@
                    [label "RNG Simulation"]))
 (define LCG-dialog
   (instantiate dialog% ("Configure Linear Gruential Generator")))
+
 (define text-m
   (new text-field%
-     [parent LCG-dialog]
-     [label "modulus m: "]
-     [init-value LCG-m]
-     [vert-margin VERT_MARGIN]))
+       [parent LCG-dialog]
+       [label "modulus m: "]
+       [init-value LCG-m]
+       [vert-margin VERT_MARGIN]))
 (define text-a
   (new text-field%
-     [parent LCG-dialog]
-     [label "multiplier a: "]
-     [init-value LCG-a]
-     [vert-margin VERT_MARGIN]))
+       [parent LCG-dialog]
+       [label "multiplier a: "]
+       [init-value LCG-a]
+       [vert-margin VERT_MARGIN]))
 (define text-c
   (new text-field%
-     [parent LCG-dialog]
-     [label "increment c: "]
-     [init-value LCG-c]
-     [vert-margin VERT_MARGIN]))
+       [parent LCG-dialog]
+       [label "increment c: "]
+       [init-value LCG-c]
+       [vert-margin VERT_MARGIN]))
 (new button%
      [parent LCG-dialog]
      [label "Save"]
      [callback (lambda (button event)
-                 )]})
-
+                 (set! text-m (send text-m get-text))
+                 (set! text-a (send text-a get-text))
+                 (set! text-c (send text-c get-text)))])
 
 (define MS-dialog
   (instantiate dialog% ("Configure Middle-Square")))
@@ -71,13 +73,13 @@
      [vert-margin VERT_MARGIN]
      [label "Configure Linear-Feedback Shift Register"]
      [callback (lambda (button event)
-                 (send LFSR-dialog show #t))]))
+                 (send LFSR-dialog show #t))])
 (new button%
      [parent frame]
      [stretchable-width 0]
      [vert-margin VERT_MARGIN]
      [label "Configure ChaCha20"]
      [callback (lambda (button event)
-                 (send ChaCha20-dialog show #t))]))
+                 (send ChaCha20-dialog show #t))])
 
 (send frame show #t)
---
+++ rng.py
@@ -0,0 +1,96 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class SettingsWindow(Gtk.Window):
+
+    LCG_m = 2**31
+    LCG_a = 1103515245
+    LCG_c = 12345
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="RNG-simulation")
+
+        vbox = Gtk.VBox(spacing=6)
+        self.add(vbox)
+
+        self.seed_hbox = Gtk.HBox()
+        vbox.pack_start(self.seed_hbox, True, True, 0)
+
+        self.seed_label = Gtk.Label(label="Seed: ")
+        self.seed_hbox.pack_start(self.seed_label, True, True, 0)
+
+        self.seed_entry = Gtk.Entry()
+        self.seed_entry.set_text("0")
+        self.seed_hbox.pack_start(self.seed_entry, True, True, 0)
+
+        self.conf_LGC = Gtk.Button.\
+            new_with_label("Configure Linear Congruential Generator")
+        vbox.pack_start(self.conf_LGC, True, True, 0)
+
+        self.conf_MS = Gtk.Button.\
+            new_with_label("Configure Middle-Square")
+        vbox.pack_start(self.conf_LGC, True, True, 0)
+
+        self.conf_LFSR = Gtk.Button.\
+            new_with_label("Configure Linear-Feedback Shift Register")
+        vbox.pack_start(self.conf_LFSR, True, True, 0)
+
+        self.conf_CC20 = Gtk.Button.\
+            new_with_label("Configure ChaCha20")
+        vbox.pack_start(self.conf_CC20, True, True, 0)
+
+        self.btn_run = Gtk.Button.new_with_label("Run Simulation")
+        self.btn_run.connect("clicked", self.on_button_run)
+        vbox.pack_start(self.btn_run, True, True, 0)
+
+    def on_button_run(self, button):
+        print(self.LCG_m, self.LCG_a, self.LCG_c)
+
+class LCGDialog(Gtk.Dialog):
+
+    def __init__(self, parent, m, a, c):
+        Gtk.Dialog.__init__(self, "Configure LCG", parent, 0,
+                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        vbox = Gtk.VBox(spacing=6)
+
+        self.m_hbox = Gtk.HBox()
+        vbox.pack_start(self.m_hbox, True, True, 0)
+        
+        self.m_label = Gtk.Label(label="modulus m: ")
+        self.m_hbox.pack_start(self.m_label, True, True, 0)
+
+        self.m_entry = Gtk.Entry()
+        self.m_entry.set_text(m)
+        self.m_hbox.pack_start(self.m_entry, True, True, 0)
+        
+        self.a_hbox = Gtk.HBox()
+        vbox.pack_start(self.a_hbox, True, True, 0)
+        
+        self.a_label = Gtk.Label(label="multiplier a: ")
+        self.a_hbox.pack_start(self.a_label, True, True, 0)
+
+        self.a_entry = Gtk.Entry()
+        self.a_entry.set_text(a)
+        self.a_hbox.pack_start(self.a_entry, True, True, 0)
+
+        self.c_hbox = Gtk.HBox()
+        vbox.pack_start(self.c_hbox, True, True, 0)
+        
+        self.c_label = Gtk.Label(label="increment c: ")
+        self.c_hbox.pack_start(self.c_label, True, True, 0)
+
+        self.c_entry = Gtk.Entry()
+        self.c_entry.set_text(c)
+        self.c_hbox.pack_start(self.c_entry, True, True, 0)
+        
+        self.add(vbox)
+        self.show_all()
+
+
+sWindow = SettingsWindow()
+sWindow.show_all()
+sWindow.connect("destroy", Gtk.main_quit)
+Gtk.main()
